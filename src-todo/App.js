c4f84fe00494e882aa5d45536383bc58db0a92c6
bimbyy <125829501+bimbyy@users.noreply.github.com>
2024-02-06 23:05:29 -0600

    Add files via upload

---
+++ src-todo/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Todo List</h1> {/* Title  here */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
